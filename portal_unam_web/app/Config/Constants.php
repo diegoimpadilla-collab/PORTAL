4099f18e96022c15522f9f72e9f9669ae21461e9
diegoimpadilla-collab <diegoimpadilla@gmail.com>
2026-05-25 17:01:15 -0500

    fix: add missing Constants.php

---
+++ portal_unam_web/app/Config/Constants.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Portal de Egresados UNAM – Constants
+ * Este archivo define constantes globales de la aplicación.
+ */
+
+defined('APP_NAMESPACE') || define('APP_NAMESPACE', 'App');
+defined('COMPOSER_PATH') || define('COMPOSER_PATH', ROOTPATH . 'vendor/autoload.php');
+defined('MINUTE') || define('MINUTE', 60);
+defined('HOUR')   || define('HOUR', 3600);
+defined('DAY')    || define('DAY', 86400);
+defined('WEEK')   || define('WEEK', 604800);
+defined('MONTH')  || define('MONTH', 2592000);
+defined('YEAR')   || define('YEAR', 31536000);
+defined('DECADE') || define('DECADE', 315360000);
